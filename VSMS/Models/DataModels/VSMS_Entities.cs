d4ac54e7ae0c16700bc01b5a7d1647072ad258c9
Thang Nguyen Huu <thangnh.cd@gmail.com>
2021-10-14 22:40:12 +0700

    commit -1
    cmt

---
+++ VSMS/Models/DataModels/VSMS_Entities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using VSMS.Models.DataModels;
+
+namespace VSMS.Models
+{
+    public class VSMS_Entities : DbContext
+    {
+        public VSMS_Entities() : base("name=VSMSConnectString")
+        {
+
+        }
+        public DbSet<Manuafature> Manuafatures { get; set; }
+        public DbSet<Mode> Modes { get; set; }
+        public DbSet<Car> Cars{ get; set; }
+        public DbSet<CarDetails> CarDetails{ get; set; }
+        public DbSet<Feature> Features{ get; set; } 
+        public DbSet<Category> Categories{ get; set; } 
+        public DbSet<DriveTest> DriveTests{ get; set; } 
+        public DbSet<Member> Members{ get; set; } 
+        public DbSet<Order> Orders{ get; set; } 
+        public DbSet<OrderDetails> OrderDetails{ get; set; } 
+        public DbSet<AdminOrder> AdminOrders{ get; set; } 
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Per_relationship> Per_Relationships { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Permission_details> Permission_Details { get; set; }
+       
+
+
+    }
+}
---
+++ VSMS/Models/Repository/Repository.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace VSMS.Models.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class, new()
+    {
+        private VSMS_Entities db;
+        private DbSet<T> tbl;
+        public Repository()
+        {
+            db = new VSMS_Entities();
+            tbl = db.Set<T>();
+        }
+        public bool Add(T entity)
+        {
+            try
+            {
+                tbl.Add(entity);
+                Save();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Edit(T entity)
+        {
+            try
+            {
+                db.Entry(entity).State = EntityState.Modified;
+                Save();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public T Get(object id)
+        {
+            return tbl.Find(id);
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return tbl.Where(predicate).AsEnumerable();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return tbl.AsEnumerable();
+        }
+
+        public bool Remove(object id)
+        {
+            try
+            {
+                tbl.Remove(Get(id));
+                Save();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Remove(T entity)
+        {
+            try
+            {
+                tbl.Remove(entity);
+                Save();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = false;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
