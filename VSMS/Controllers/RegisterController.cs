5cbd88f8ea337e4b71f7bbb788822467ef57c73e
Khắc Duyệt <duyetbk.nh@gmail.com>
2021-10-31 23:52:29 +0700

    11h52 31/10/2021
    11h52 31/10/2021

--- VSMS/Controllers/RegisterController.cs
+++ VSMS/Controllers/RegisterController.cs
@@ -1,5 +1,4 @@
-﻿using NPOI.POIFS.Crypt;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -25,7 +24,7 @@ namespace VSMS.Controllers
         [HttpPost]
         public ActionResult Login([Bind(Include = "UserName, PassWord")] Member mem)
         {
-            var md5 = Encryptor(mem.PassWord);
+            var md5 = Common.CommonConstants.ParseMD5(mem.PassWord);
             var cus = db.Members.SingleOrDefault(x => x.UserName == mem.UserName && x.PassWord == md5);
             if (cus != null)
             {
